4880743acd20f3c2b83d6fec4ed4edbe7a2a00c6
Joey Huab <jhuab@edukasyon.ph>
2025-06-12 20:01:31 +0900

    Revert "lineage: overlay: Exclude backup transport overlays from RRO."
    This reverts commit c4535991642fb4e0644f560a8bb28e538399d492.

--- config/common.mk
+++ config/common.mk
@@ -306,10 +306,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
     setupwizard.feature.day_night_mode_enabled=true
 endif
 
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/lineage/overlay/no-rro
-PRODUCT_PACKAGE_OVERLAYS += \
-    vendor/lineage/overlay/common \
-    vendor/lineage/overlay/no-rro
+PRODUCT_PACKAGE_OVERLAYS += vendor/lineage/overlay/common
 
 PRODUCT_PACKAGES += \
     DocumentsUIOverlay \
